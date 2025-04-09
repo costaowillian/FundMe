03b7560063616995ff56ba2a1316a67ac43448f5
WillianCosta <willian95@outlook.com>
2025-04-09 13:47:03 -0300

    feat: add contract DeployFundMe to handle with FundMe delpoy

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployFundMe is Script {
+    FundMe public fundMe;
+
+    function setUp() public {}
+
+    function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+
+        return fundMe;
+    }
+}
